db52e0571a7247429c7e0b12d75037d3f330584f
root <root@_HOSTNAME_>
2023-02-25 05:14:29 +0100

    add rewards

--- README.md
+++ README.md
@@ -1,27 +1,37 @@
+This a fork from https://github.com/Ziltar/HOPR_Node_Notification. Great thanks to Ziltar!
+
 # HOPR Node Notification
 Simple script to get the status of a node via Telegram. 
 
 ## Telegram bot Node Notification
 
-This bot checks the availability of the node (or multiple nodes) every hour and sends a notification about the status of the node(s). A notification can be sent always or only when the availability changes. 
+This bot checks the availability of the node (or multiple nodes) every certain time and sends a notification about the status of the node(s). A notification can be sent always or only when the availability changes. 
 
 ## Installation
 
-1. Clone the repository : <br> `git clone https://github.com/Ziltar/HOPR_Node_Notification.git && cd HOPR_Node_Notification` 
-2. Install requirements:  <br> `pip install requirements.txt `
+1. Clone the repository : <br> `git clone https://github.com/alpha7b/HOPR_Node_Notification.git && cd HOPR_Node_Notification` 
+2. Install requirements:  <br> `pip install -r requirements.txt `
 3. Rename `setting.py_sample` to `setting.py`
-4. Edit the *settings.py* File: <br>
+4. Edit the *settings.py* File: <br><br>
 <b>NODES:</b> HOPR Node Address(es)<br>
-*Example:*<br> `NODES = ["16Uiu2HAmH3RhgTDN6cwysRnMtMf4jfzsvKuyuKPu5iDRZUZiVsqx", "16Uiu2HAkuTsuFSak6UJeF2w3KV2uzoNSpuGNFKC2vyLt63BpwuDC"]`<br>
+*Example:*<br> `NODES = ["16Uiu2HAmH3RhgTDN6cwysRnMtMf4jfzsvKuyuKPu5iDRZUZiVsqx", "16Uiu2HAkuTsuFSak6UJeF2w3KV2uzoNSpuGNFKC2vyLt63BpwuDC"]`<br><br>
 <b>TELEGRAM_TOKEN</b><br>
 *Example:* <br>`TELEGRAM_TOKEN  =  "5475148206:ABBc0fhQSwfBjKioPwbaZ9uxEPkpsgqbgi"`<br>
-You can get your token here: https://t.me/BotFather <br>
+You can get your token here: https://t.me/BotFather <br><br>
 <b>TELEGRAM_ID</b><br>
 You can use a bot to find out your ID: https://t.me/userinfobot<br>
-*Example:*<br> `TELEGRAM_ID  =  "15915301634711"`<br>
+*Example:*<br> `TELEGRAM_ID  =  "15915301634711"`<br><br>
 <b>ON_CHANGE</b><br>
-If True, the bot sends a notification only when the availability changes (24 hours). If False, a notification is sent every hour.<br>
-*Example:* <br>`ON_CHANGE  =  True`
+If True, the bot sends a notification only when the availability changes (24 hours). If False, a notification is sent 30 minutes.<br>
+*Example:* <br>`ON_CHANGE  =  True`<br><br>
+<b>INTERVAL = '1800'</b><br>
+By default, the bot will check node status every 1800 seconds which is 30 minutes. You can change the interval. For example, change to 3600 seconds.<br>
+*Example:* <br>`INTERVAL  =  '3600'`<br><br>
+<b>ON_REWARD_CHANGE</b><br>
+If True, the bot sends a notification when reward changes.<br>
+*Example:* <br>`ON_REWARD_CHANGE  =  True`<br><br>
+<b>STAKE_WALLETS:</b> For reward status:<br>
+*Example:*<br> `STAKE_WALLETS = ["0x0000000000000000000000000000000000000000", "0x0000000000000000000000000000000000000001"]`<br>
 
 ## Usage
 Run main.py: `python3 main.py` <br><br>
--- main.py
+++ main.py
@@ -1,8 +1,10 @@
-from settings import TELEGRAM_TOKEN, TELEGRAM_ID, NODES, ON_CHANGE
-import requests, time
+from settings import TELEGRAM_TOKEN, TELEGRAM_ID, NODES, ON_CHANGE, INTERVAL, STAKE_WALLETS, ON_REWARD_CHANGE
+import requests, time, pandas
 
 URL = "https://network.hoprnet.org/api/getNodes?env=36"
+REWARD_URL = "https://stake.hoprnet.org/api/getRewards?peerId="
 stats = {}
+reward = 0
 
 while True:
     try:
@@ -17,20 +19,36 @@ while True:
                             change = True
                     else:
                         change = True
-                    print("node['availability24h'] is: " + str(node["availability24h"]))
+                    print("node['availability24h'] is: " + str(node["availability24h"]) + "\n\n")
+                    print("node['nextEstRewards'] is: " + str(node["nextEstRewards"]) + "\n\n")
                     stats[node["peerId"]] =  node["availability24h"]
-                    print("current stats is: ")
-                    print(stats)    
+                    print("current stats is: " + str(stats) + "\n\n")
+                    
                 now = time.time()
                 lastSeenInMinute = (now - node["lastSeen"]/1000)/60                
-                msg += "Node: " + node["peerId"] + "\nAvailability: " + str(format(node["availability"]*100, '.2f')) + "%\nAvailability 24h: " + str(format(node["availability24h"]*100, '.2f')) + "%\nLatency:" + str(format(node["latencyAverage"], '.2f')) + "ms\nLastSeen:" + str(format(lastSeenInMinute, '.2f')) + "min\n\n"
-                print("msg is: \n" + msg)
-        print(change)
+                msg += "Node: " + node["peerId"] + "\nAvailability: " + str(format(node["availability"]*100, '.2f')) + "%\nAvailability 24h: " + str(format(node["availability24h"]*100, '.2f')) + "%\nLatency:" + str(format(node["latencyAverage"], '.2f')) + "ms\nLastSeen:" + str(format(lastSeenInMinute, '.2f')) + "min\nnextEstRewards:" + str(format(node["nextEstRewards"], '.2f')) + " HOPR\n\n"
+                print("msg is: \n" + msg + "\n\n")
+        print("Is status changed? " + str(change))
+
+        total_reward = 0
+        for address in STAKE_WALLETS:
+            response = requests.request("GET", REWARD_URL+address)
+            total_reward += (response.json()[0]['rewards'])
+        msg+= "\n💸Totoal Reward:" + str(total_reward) + " HOPR\n\n"
+        print("msg with reward is: \n" + msg)
+
         if (not ON_CHANGE) or (ON_CHANGE and change):
             url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage?chat_id={TELEGRAM_ID}&parse_mode=HTML&text=<code>{msg}</code>"
             requests.get(url)
-    except:
+        elif (ON_REWARD_CHANGE and (reward  != total_reward)):
+            msg = "💸Reward:" + str(total_reward) + " HOPR"
+            url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage?chat_id={TELEGRAM_ID}&parse_mode=HTML&text=<code>{msg}</code>"
+            requests.get(url)
+            reward = total_reward
+
+    except Exception as e:
+        print(str(e))
         print("something went wrong")
         url = f"https://api.telegram.org/bot{TELEGRAM_TOKEN}/sendMessage?chat_id={TELEGRAM_ID}&parse_mode=HTML&text=<code>Something went wrong!</code>"
         requests.get(url)        
-    time.sleep(1800)
+    time.sleep(int(INTERVAL))
